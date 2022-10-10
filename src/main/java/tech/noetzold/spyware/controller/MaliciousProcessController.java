1f492cbf1615d0b3a9c309d364430a529a6fb1f6
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-10-10 06:50:39 -0300

    Create MaliciousProcessController.java

---
+++ src/main/java/tech/noetzold/spyware/controller/MaliciousProcessController.java
@@ -0,0 +1,54 @@
+package tech.noetzold.spyware.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import tech.noetzold.spyware.model.MaliciousProcess;
+import tech.noetzold.spyware.service.MaliciousProcessService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collection;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/process")
+public class MaliciousProcessController {
+    @Autowired
+    MaliciousProcessService maliciousProcessService;
+
+    @RequestMapping(value = "/getAll", method = RequestMethod.GET)
+    @Transactional
+    public ResponseEntity<Collection<MaliciousProcess>> getAll(HttpServletRequest request, HttpServletResponse response) {
+        return new ResponseEntity<Collection<MaliciousProcess>>(maliciousProcessService.findAll(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/get/{nome}", method = RequestMethod.GET)
+    @Transactional
+    public ResponseEntity<MaliciousProcess> getMaliciousProcessById(@PathVariable("nome") long id) {
+
+        MaliciousProcess maliciousProcess = maliciousProcessService.findById(id).get();
+
+        return new ResponseEntity<MaliciousProcess>(maliciousProcess, HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public ResponseEntity<MaliciousProcess> save(@RequestBody MaliciousProcess maliciousProcess) {
+        try {
+            maliciousProcess = maliciousProcessService.save(maliciousProcess);
+            return new ResponseEntity<MaliciousProcess>(maliciousProcess, HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<MaliciousProcess>(maliciousProcess, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @GetMapping("remover/{id}")
+    public String remover(@PathVariable("id") Long id) {
+        maliciousProcessService.deleteById(id);
+        return "redirect:/home";
+    }
+}
