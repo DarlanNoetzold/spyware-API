ff35efe71a706160223c9d868a62aea4f2f0facf
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-10-10 06:50:41 -0300

    Create MaliciousWebsiteController.java

---
+++ src/main/java/tech/noetzold/spyware/controller/MaliciousWebsiteController.java
@@ -0,0 +1,54 @@
+package tech.noetzold.spyware.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import tech.noetzold.spyware.model.MaliciousWebsite;
+import tech.noetzold.spyware.service.MaliciousWebsiteService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collection;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/website")
+public class MaliciousWebsiteController {
+    @Autowired
+    MaliciousWebsiteService maliciousWebsiteService;
+
+    @RequestMapping(value = "/getAll", method = RequestMethod.GET)
+    @Transactional
+    public ResponseEntity<Collection<MaliciousWebsite>> getAll(HttpServletRequest request, HttpServletResponse response) {
+        return new ResponseEntity<Collection<MaliciousWebsite>>(maliciousWebsiteService.findAll(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/get/{nome}", method = RequestMethod.GET)
+    @Transactional
+    public ResponseEntity<MaliciousWebsite> getMaliciousWebsiteById(@PathVariable("nome") long id) {
+
+        MaliciousWebsite maliciousWebsite = maliciousWebsiteService.findById(id).get();
+
+        return new ResponseEntity<MaliciousWebsite>(maliciousWebsite, HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public ResponseEntity<MaliciousWebsite> save(@RequestBody MaliciousWebsite maliciousWebsite) {
+        try {
+            maliciousWebsite = maliciousWebsiteService.save(maliciousWebsite);
+            return new ResponseEntity<MaliciousWebsite>(maliciousWebsite, HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<MaliciousWebsite>(maliciousWebsite, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @GetMapping("remover/{id}")
+    public String remover(@PathVariable("id") Long id) {
+        maliciousWebsiteService.deleteById(id);
+        return "redirect:/home";
+    }
+}
