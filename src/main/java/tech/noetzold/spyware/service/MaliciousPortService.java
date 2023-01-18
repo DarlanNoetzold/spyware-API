f534c78729260a5545303104f7d9ee96eebea29c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-18 19:47:12 -0300

    Update MaliciousPortService.java

--- src/main/java/tech/noetzold/spyware/service/MaliciousPortService.java
+++ src/main/java/tech/noetzold/spyware/service/MaliciousPortService.java
@@ -1,7 +1,31 @@
 package tech.noetzold.spyware.service;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import tech.noetzold.spyware.model.MaliciousPort;
+import tech.noetzold.spyware.repository.MaliciousPortRepository;
 
-public interface MaliciousPortService extends JpaRepository<MaliciousPort, Long> {
+import java.util.List;
+
+@Service
+public class MaliciousPortService {
+
+    @Autowired
+    MaliciousPortRepository maliciousPortRepository;
+
+    public List<MaliciousPort> findAllMaliciousPort(){
+        return maliciousPortRepository.findAll();
+    }
+
+    public MaliciousPort findMaliciousPortById(Long id){
+        return maliciousPortRepository.findById(id).get();
+    }
+
+    public MaliciousPort saveMaliciousPort(MaliciousPort maliciousPort){
+        return maliciousPortRepository.save(maliciousPort);
+    }
+
+    public void deleteMaliciousPortById(Long id){
+        maliciousPortRepository.deleteById(id);
+    }
 }
