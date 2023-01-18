9efc61e5c3738a3a4769b6929e389ef662bb0638
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-18 19:47:16 -0300

    Update MaliciousWebsiteService.java

--- src/main/java/tech/noetzold/spyware/service/MaliciousWebsiteService.java
+++ src/main/java/tech/noetzold/spyware/service/MaliciousWebsiteService.java
@@ -1,7 +1,31 @@
 package tech.noetzold.spyware.service;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import tech.noetzold.spyware.model.MaliciousWebsite;
+import tech.noetzold.spyware.repository.MaliciousWebsiteRepository;
 
-public interface MaliciousWebsiteService extends JpaRepository<MaliciousWebsite, Long> {
+import java.util.List;
+
+@Service
+public class MaliciousWebsiteService {
+
+    @Autowired
+    MaliciousWebsiteRepository maliciousWebsiteRepository;
+
+    public List<MaliciousWebsite> findAllMaliciousWebsite(){
+        return maliciousWebsiteRepository.findAll();
+    }
+
+    public MaliciousWebsite findMaliciousWebsiteById(Long id){
+        return maliciousWebsiteRepository.findById(id).get();
+    }
+
+    public MaliciousWebsite saveMaliciousWebsite(MaliciousWebsite maliciousPort){
+        return maliciousWebsiteRepository.save(maliciousPort);
+    }
+
+    public void deleteWebsiteById(Long id){
+        maliciousWebsiteRepository.deleteById(id);
+    }
 }
