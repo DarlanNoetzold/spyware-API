80dc7d5c1e847c4759ac810ac6a443e296a42dbc
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-18 19:47:04 -0300

    Update BadLanguageService.java

--- src/main/java/tech/noetzold/spyware/service/BadLanguageService.java
+++ src/main/java/tech/noetzold/spyware/service/BadLanguageService.java
@@ -1,7 +1,31 @@
 package tech.noetzold.spyware.service;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import tech.noetzold.spyware.model.BadLanguage;
+import tech.noetzold.spyware.repository.BadLanguageRepository;
 
-public interface BadLanguageService extends JpaRepository<BadLanguage, Long> {
+import java.util.List;
+
+@Service
+public class BadLanguageService {
+
+    @Autowired
+    BadLanguageRepository badLanguageRepository;
+
+    public List<BadLanguage> findAllBadLanguage(){
+        return badLanguageRepository.findAll();
+    }
+
+    public BadLanguage findBadLanguageById(Long id){
+        return badLanguageRepository.findById(id).get();
+    }
+
+    public BadLanguage saveBadLanguage(BadLanguage badLanguage){
+        return badLanguageRepository.save(badLanguage);
+    }
+
+    public void deleteBadLanguage(Long id){
+        badLanguageRepository.deleteById(id);
+    }
 }
