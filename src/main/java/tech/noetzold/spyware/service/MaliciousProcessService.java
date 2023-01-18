bdb25b45502ce00e4270335f8bcc208c9b5bb063
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-18 19:47:14 -0300

    Update MaliciousProcessService.java

--- src/main/java/tech/noetzold/spyware/service/MaliciousProcessService.java
+++ src/main/java/tech/noetzold/spyware/service/MaliciousProcessService.java
@@ -1,7 +1,32 @@
 package tech.noetzold.spyware.service;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import tech.noetzold.spyware.model.MaliciousProcess;
+import tech.noetzold.spyware.repository.MaliciousProcessRepository;
 
-public interface MaliciousProcessService extends JpaRepository<MaliciousProcess, Long> {
+import java.util.List;
+
+@Service
+public class MaliciousProcessService {
+
+    @Autowired
+    MaliciousProcessRepository maliciousProcessRepository;
+
+    public List<MaliciousProcess> findAllMaliciousProcess(){
+        return maliciousProcessRepository.findAll();
+    }
+
+    public MaliciousProcess findMaliciousProcessById(Long id){
+        return maliciousProcessRepository.findById(id).get();
+    }
+
+    public MaliciousProcess saveMaliciousProcess(MaliciousProcess maliciousPort){
+        return maliciousProcessRepository.save(maliciousPort);
+    }
+
+    public void deleteProcessById(Long id){
+        maliciousProcessRepository.deleteById(id);
+    }
+
 }
