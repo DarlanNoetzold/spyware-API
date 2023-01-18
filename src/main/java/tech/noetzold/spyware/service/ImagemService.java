29ed378a3e04b461a22b5bc62ade78876b8491b2
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-18 19:47:09 -0300

    Update ImagemService.java

--- src/main/java/tech/noetzold/spyware/service/ImagemService.java
+++ src/main/java/tech/noetzold/spyware/service/ImagemService.java
@@ -1,7 +1,31 @@
 package tech.noetzold.spyware.service;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import tech.noetzold.spyware.model.Imagem;
+import tech.noetzold.spyware.repository.ImagemRepository;
 
-public interface ImagemService extends JpaRepository<Imagem, Long> {
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ImagemService {
+    @Autowired
+    ImagemRepository imagemRepository;
+
+    public List<Imagem> findAllImages(){
+        return imagemRepository.findAll();
+    }
+
+    public Imagem findImagemById(Long id){
+        return imagemRepository.findById(id).get();
+    }
+
+    public Imagem saveImagem(Imagem imagem){
+        return imagemRepository.save(imagem);
+    }
+
+    public void deleteImagem(Imagem imagem){
+        imagemRepository.delete(imagem);
+    }
 }
