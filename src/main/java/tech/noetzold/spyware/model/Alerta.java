b57e3e98cc0f92169e485a61877952b9d5d2890b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-20 07:21:10 -0300

    Update Alerta.java

--- src/main/java/tech/noetzold/spyware/model/Alerta.java
+++ src/main/java/tech/noetzold/spyware/model/Alerta.java
@@ -6,11 +6,12 @@ import org.hibernate.annotations.Type;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Set;
 
 @Entity
-public class Alerta {
+public class Alerta implements Serializable {
     @Id
     @GeneratedValue(generator = "increment")
     @GenericGenerator(name = "increment", strategy = "increment")
