5021dab0c8aff3a84c933b41a8dd7ca30fb48073
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-20 07:21:05 -0300

    Create RedisConfig.java

---
+++ src/main/java/tech/noetzold/spyware/cache/RedisConfig.java
@@ -0,0 +1,24 @@
+package tech.noetzold.spyware.cache;
+
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+        return (builder) -> {
+            Map<String, RedisCacheConfiguration> configurationMap = new HashMap<>();
+            configurationMap.put("alerta", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(20)));
+            builder.withInitialCacheConfigurations(configurationMap);
+        };
+    }
+
+}
