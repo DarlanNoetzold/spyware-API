ca576eca9fb77815c561685025688bda74131d1a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-20 07:21:25 -0300

    Create AlertaRepositoryTest.java

---
+++ src/test/java/tech/noetzold/spyware/repository/AlertaRepositoryTest.java
@@ -0,0 +1,15 @@
+package tech.noetzold.spyware.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+
+@DataJpaTest
+@DisplayName("Tests for Alerta Repository")
+@Log4j2
+class AlertaRepositoryTest {
+
+
+}
