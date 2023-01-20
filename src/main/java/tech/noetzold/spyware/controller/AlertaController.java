24bd00b48c9df5f4d56890f7f584a0a9b3c2698c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-20 07:21:07 -0300

    Update AlertaController.java

--- src/main/java/tech/noetzold/spyware/controller/AlertaController.java
+++ src/main/java/tech/noetzold/spyware/controller/AlertaController.java
@@ -1,6 +1,7 @@
 package tech.noetzold.spyware.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
