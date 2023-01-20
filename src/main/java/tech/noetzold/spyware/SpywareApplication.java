0ea9f96253dc053aa471e87701e21912c83c40f1
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-01-20 07:21:18 -0300

    Update SpywareApplication.java

--- src/main/java/tech/noetzold/spyware/SpywareApplication.java
+++ src/main/java/tech/noetzold/spyware/SpywareApplication.java
@@ -2,11 +2,13 @@ package tech.noetzold.spyware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
+@EnableCaching
 @SpringBootApplication
 public class SpywareApplication {
 
