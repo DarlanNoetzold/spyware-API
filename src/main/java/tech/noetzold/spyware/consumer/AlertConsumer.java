115126905bfa66cf5bfb5adf6b36de6f741fb2ea
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-02-19 15:02:57 -0300

    New version of the aplication

---
+++ src/main/java/tech/noetzold/spyware/consumer/AlertConsumer.java
@@ -0,0 +1,26 @@
+package tech.noetzold.spyware.consumer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import tech.noetzold.spyware.message.RabbitmqConstantes;
+import tech.noetzold.spyware.model.Alert;
+import tech.noetzold.spyware.service.AlertService;
+
+import javax.transaction.Transactional;
+
+@Component
+public class AlertConsumer {
+
+    @Autowired
+    AlertService alertService;
+
+    @Transactional
+    @RabbitListener(queues = RabbitmqConstantes.FILA_ALERTA)
+    private void cnsumerAlerta(String mensagem) throws JsonProcessingException {
+        Alert alert = new ObjectMapper().readValue(mensagem, Alert.class);
+        alertService.saveAlerta(alert);
+    }
+}
