18b788deeb1f5e4a3fba4b676094bb97dee3a196
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-02-22 07:12:01 -0300

    .

--- src/main/java/tech/noetzold/spyware/repository/ImagemRepository.java
+++
@@ -1,7 +0,0 @@
-package tech.noetzold.spyware.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.noetzold.spyware.model.Imagem;
-
-public interface ImagemRepository extends JpaRepository<Imagem, Long> {
-}
