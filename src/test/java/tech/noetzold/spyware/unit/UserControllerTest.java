d57bbb9f89b3a8ccf46020ee039b3ca390c0a4ff
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-04-11 21:03:08 -0300

    Create UserControllerTest.java

---
+++ src/test/java/tech/noetzold/spyware/unit/UserControllerTest.java
@@ -0,0 +1,9 @@
+package tech.noetzold.spyware.unit;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import tech.noetzold.spyware.controller.UserController;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+}
