435ba60bcaf781038c32cbe35118665ec71aeb05
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-04-11 21:03:06 -0300

    Create MaliciousWebsiteControllerTest.java

---
+++ src/test/java/tech/noetzold/spyware/unit/MaliciousWebsiteControllerTest.java
@@ -0,0 +1,8 @@
+package tech.noetzold.spyware.unit;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import tech.noetzold.spyware.controller.MaliciousWebsiteController;
+
+@WebMvcTest(MaliciousWebsiteController.class)
+public class MaliciousWebsiteControllerTest {
+}
