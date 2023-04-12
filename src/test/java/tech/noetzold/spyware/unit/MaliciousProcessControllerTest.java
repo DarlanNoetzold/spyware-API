2ce09defd7da07da29b127ac0fd00c03b7ceb6b2
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-04-11 21:03:04 -0300

    Create MaliciousProcessControllerTest.java

---
+++ src/test/java/tech/noetzold/spyware/unit/MaliciousProcessControllerTest.java
@@ -0,0 +1,8 @@
+package tech.noetzold.spyware.unit;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import tech.noetzold.spyware.controller.MaliciousProcessController;
+
+@WebMvcTest(MaliciousProcessController.class)
+public class MaliciousProcessControllerTest {
+}
