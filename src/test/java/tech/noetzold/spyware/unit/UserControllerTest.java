7442a7af7ae2baa2946b370b764855f52910c917
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-04-13 07:51:36 -0300

    Delete UserControllerTest.java

--- src/test/java/tech/noetzold/spyware/unit/UserControllerTest.java
+++
@@ -1,9 +0,0 @@
-package tech.noetzold.spyware.unit;
-
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import tech.noetzold.spyware.controller.UserController;
-
-@WebMvcTest(UserController.class)
-public class UserControllerTest {
-
-}
