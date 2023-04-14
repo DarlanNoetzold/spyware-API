5bea5fc06471fd9d0e05c4a7ea836987ff714f4a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-04-14 20:04:13 -0300

    Update AlertController.java

--- src/main/java/tech/noetzold/spyware/controller/AlertController.java
+++ src/main/java/tech/noetzold/spyware/controller/AlertController.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import tech.noetzold.spyware.message.RabbitmqConstantes;
 import tech.noetzold.spyware.model.Alert;
+import tech.noetzold.spyware.model.Image;
 import tech.noetzold.spyware.repository.ImageRepository;
 import tech.noetzold.spyware.service.AlertService;
 import tech.noetzold.spyware.service.RabbitmqService;
@@ -18,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 
 @CrossOrigin
@@ -37,50 +39,66 @@ public class AlertController {
     @Transactional
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<Page<Alert>> getAll(HttpServletRequest request, HttpServletResponse response, Pageable pageable) {
-        return new ResponseEntity<>(alertaSevice.findAll(pageable), HttpStatus.OK);
+        if (pageable == null || pageable.getPageNumber() < 0 || pageable.getPageSize() <= 0) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Page<Alert> alerts = alertaSevice.findAll(pageable);
+        return new ResponseEntity<>(alerts, HttpStatus.OK);
     }
 
     @Transactional
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Alert> getAlertaId(@PathVariable("id") long id) {
-        try {
-            if(id <= 0)
-                return new ResponseEntity<Alert>(HttpStatus.BAD_REQUEST);
-
-            Alert alert = alertaSevice.findAlertaById(id);
-
-            return new ResponseEntity<Alert>(alert, HttpStatus.OK);
-        }catch (Exception e){
-            e.printStackTrace();
-            return new ResponseEntity<Alert>(HttpStatus.NOT_FOUND);
+        if (id <= 0) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+        Alert alert = alertaSevice.findAlertaById(id);
+        if (alert == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(alert, HttpStatus.OK);
     }
 
     @Transactional
     @RequestMapping(value = "/pcId/{pcId}", method = RequestMethod.GET)
     public ResponseEntity<Collection<Alert>> getAlertaPcId(@PathVariable("pcId") String pcId) {
-
+        if (pcId == null || pcId.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         List<Alert> alerts = alertaSevice.findAlertaByPcId(pcId);
-
-        return new ResponseEntity<Collection<Alert>>(alerts, HttpStatus.OK);
+        if (alerts == null || alerts.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(alerts, HttpStatus.OK);
     }
 
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public ResponseEntity<Alert> save(@RequestBody Alert alert, HttpServletRequest request, HttpServletResponse response) {
-        try {
-            alert.setImagem(imageRepository.findById(alert.getImagem().getId()).get());
-            alert.setData_cadastro(Calendar.getInstance());
-            this.rabbitmqService.enviaMensagem(RabbitmqConstantes.FILA_ALERT, alert);
-            alert.getImagem().setBase64Img("");
-            return new ResponseEntity<Alert>(alert, HttpStatus.CREATED);
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (alert == null || alert.getImagem() == null ||
+                alert.getImagem().getId() == null || alert.getData_cadastro() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<Alert>(alert, HttpStatus.UNPROCESSABLE_ENTITY);
+        Optional<Image> optionalImage = imageRepository.findById(alert.getImagem().getId());
+        if (!optionalImage.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        alert.setImagem(optionalImage.get());
+        alert.setData_cadastro(Calendar.getInstance());
+        this.rabbitmqService.enviaMensagem(RabbitmqConstantes.FILA_ALERT, alert);
+        alert.getImagem().setBase64Img("");
+        return new ResponseEntity<>(alert, HttpStatus.CREATED);
     }
+
     @DeleteMapping("remove/{id}")
-    public String remover(@PathVariable("id") Long id) {
+    public String remove(@PathVariable("id") Long id) {
+        if (id == null || id <= 0) {
+            return "redirect:/home?error=InvalidAlertId";
+        }
+        Alert alert = alertaSevice.findAlertaById(id);
+        if (alert == null) {
+            return "redirect:/home?error=AlertNotFound";
+        }
         alertaSevice.deleteAlertaById(id);
         return "redirect:/home";
     }
