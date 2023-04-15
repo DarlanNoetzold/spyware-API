adbe315e87cf35cb2ca8cd48b79559aec3bad917
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-04-15 09:28:42 -0300

    Update MaliciousPortRepository.java

--- src/main/java/tech/noetzold/spyware/repository/MaliciousPortRepository.java
+++ src/main/java/tech/noetzold/spyware/repository/MaliciousPortRepository.java
@@ -3,5 +3,8 @@ package tech.noetzold.spyware.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.noetzold.spyware.model.MaliciousPort;
 
+import java.util.Optional;
+
 public interface MaliciousPortRepository extends JpaRepository<MaliciousPort, Long> {
+    Optional<MaliciousPort> findByVulnarableBanners(String vulnarableBanners);
 }
