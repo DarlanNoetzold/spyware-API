9777d61841d24e08433016a710b30134aee2f3ee
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-04-15 09:28:36 -0300

    Update MaliciousProcessController.java

--- src/main/java/tech/noetzold/spyware/controller/MaliciousProcessController.java
+++ src/main/java/tech/noetzold/spyware/controller/MaliciousProcessController.java
@@ -22,34 +22,41 @@ public class MaliciousProcessController {
     @RequestMapping(value = "/getAll", method = RequestMethod.GET)
     @Transactional
     public ResponseEntity<Collection<MaliciousProcess>> getAll(HttpServletRequest request, HttpServletResponse response) {
-        return new ResponseEntity<Collection<MaliciousProcess>>(maliciousProcessService.findAllMaliciousProcess(), HttpStatus.OK);
+        Collection<MaliciousProcess> maliciousProcesses = maliciousProcessService.findAllMaliciousProcess();
+        if (maliciousProcesses.isEmpty()) {
+            return new ResponseEntity<Collection<MaliciousProcess>>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<Collection<MaliciousProcess>>(maliciousProcesses, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
     @Transactional
     public ResponseEntity<MaliciousProcess> getMaliciousProcessById(@PathVariable("id") long id) {
-        try {
-            if (id <= 0)
-                return new ResponseEntity<MaliciousProcess>(HttpStatus.BAD_REQUEST);
-            MaliciousProcess maliciousProcess = maliciousProcessService.findMaliciousProcessById(id);
-
-            return new ResponseEntity<MaliciousProcess>(maliciousProcess, HttpStatus.OK);
-        }catch (NullPointerException e){
-            e.printStackTrace();
+        if (id <= 0) {
+            return new ResponseEntity<MaliciousProcess>(HttpStatus.BAD_REQUEST);
+        }
+        MaliciousProcess maliciousProcess = maliciousProcessService.findMaliciousProcessById(id);
+        if (maliciousProcess == null) {
             return new ResponseEntity<MaliciousProcess>(HttpStatus.NOT_FOUND);
         }
+        return new ResponseEntity<MaliciousProcess>(maliciousProcess, HttpStatus.OK);
     }
 
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public ResponseEntity<MaliciousProcess> save(@RequestBody MaliciousProcess maliciousProcess) {
-        try {
-            maliciousProcess = maliciousProcessService.saveMaliciousProcess(maliciousProcess);
-            return new ResponseEntity<MaliciousProcess>(maliciousProcess, HttpStatus.CREATED);
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (maliciousProcess == null) {
+            return new ResponseEntity<MaliciousProcess>(HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<MaliciousProcess>(maliciousProcess, HttpStatus.UNPROCESSABLE_ENTITY);
+
+        MaliciousProcess existingMaliciousProcess = maliciousProcessService.findMaliciousProcessByNameExe(maliciousProcess.getNameExe());
+
+        if(existingMaliciousProcess != null){
+            return new ResponseEntity<MaliciousProcess>(existingMaliciousProcess, HttpStatus.CREATED);
+        }
+
+        maliciousProcess = maliciousProcessService.saveMaliciousProcess(maliciousProcess);
+        return new ResponseEntity<MaliciousProcess>(maliciousProcess, HttpStatus.CREATED);
     }
 
     @DeleteMapping("remove/{id}")
