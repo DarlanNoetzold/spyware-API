7a87d3c5ba17a7e3583545593b6d9d3c0866fef9
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-04-15 07:54:22 -0300

    Update BadLanguageRepository.java

--- src/main/java/tech/noetzold/spyware/repository/BadLanguageRepository.java
+++ src/main/java/tech/noetzold/spyware/repository/BadLanguageRepository.java
@@ -3,5 +3,8 @@ package tech.noetzold.spyware.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.noetzold.spyware.model.BadLanguage;
 
+import java.util.Optional;
+
 public interface BadLanguageRepository extends JpaRepository<BadLanguage, Long> {
+    Optional<BadLanguage> findByWord(String word);
 }
