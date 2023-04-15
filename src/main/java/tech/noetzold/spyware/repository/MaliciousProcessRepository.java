ba523a14c731b9ff213a17c059b414f99fdc1309
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-04-15 09:28:44 -0300

    Update MaliciousProcessRepository.java

--- src/main/java/tech/noetzold/spyware/repository/MaliciousProcessRepository.java
+++ src/main/java/tech/noetzold/spyware/repository/MaliciousProcessRepository.java
@@ -3,5 +3,8 @@ package tech.noetzold.spyware.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.noetzold.spyware.model.MaliciousProcess;
 
+import java.util.Optional;
+
 public interface MaliciousProcessRepository extends JpaRepository<MaliciousProcess, Long> {
+    Optional<MaliciousProcess> findByNameExe(String nameExe);
 }
