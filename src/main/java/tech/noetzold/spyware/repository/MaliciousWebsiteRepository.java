ba415e28baeb037b0b8f484947369a8804c65e3c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-04-15 09:28:45 -0300

    Update MaliciousWebsiteRepository.java

--- src/main/java/tech/noetzold/spyware/repository/MaliciousWebsiteRepository.java
+++ src/main/java/tech/noetzold/spyware/repository/MaliciousWebsiteRepository.java
@@ -3,5 +3,8 @@ package tech.noetzold.spyware.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.noetzold.spyware.model.MaliciousWebsite;
 
+import java.util.Optional;
+
 public interface MaliciousWebsiteRepository extends JpaRepository<MaliciousWebsite, Long> {
+    Optional<MaliciousWebsite> findByUrl(String url);
 }
