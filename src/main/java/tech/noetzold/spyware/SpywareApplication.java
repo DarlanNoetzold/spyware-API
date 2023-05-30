01c0f25007bebf0af1f9fe64ac0f490fb1a1b848
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-05-30 06:56:04 -0300

    Update SpywareApplication.java

--- src/main/java/tech/noetzold/spyware/SpywareApplication.java
+++ src/main/java/tech/noetzold/spyware/SpywareApplication.java
@@ -3,7 +3,6 @@ package tech.noetzold.spyware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,7 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableCaching
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {JacksonAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {})
 public class SpywareApplication {
 
 	public static void main(String[] args) {
