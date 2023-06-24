adc72d8c2036d54fb8fafc3b0fe4e07b3dc36f1e
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-06-23 21:52:55 -0300

    Update ImageControllerTest.java

--- src/test/java/tech/noetzold/spyware/unit/ImageControllerTest.java
+++ src/test/java/tech/noetzold/spyware/unit/ImageControllerTest.java
@@ -81,7 +81,7 @@ public class ImageControllerTest {
         mockMvc.perform(post("/image/save")
                         .header("Authorization", "Bearer " + generateToken())
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{ \"productImg\": \"teste\", \"base64Img\": \"biVBORw0KGgoAAAANSUhEUgAADwAAAAhwCAIAAAAf3FwlAAEAAElEQVR4nOzdfVxU173o/zWAglt0BBwUErkJggg=\" }"))
+                        .content("{ \"productImg\": \"teste\", \"base64Img\": \"iVBORw0KGgoAAAANSUhEUgAADwAAAAhwCAIAAAAf3FwlAAEAAElEQVR4nOzdfVxU173o/zWAglt0BBwUErkJggg=\" }"))
                 .andExpect(status().is4xxClientError())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
