9bbfec23a7c0c558cf25bc7cfe436acc8b5515b0
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-06-28 14:01:06 -0300

    Update ImageController.java

--- src/main/java/tech/noetzold/spyware/controller/ImageController.java
+++ src/main/java/tech/noetzold/spyware/controller/ImageController.java
@@ -1,8 +1,8 @@
 package tech.noetzold.spyware.controller;
 
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +22,7 @@ public class ImageController {
     @Autowired
     ImageService imageService;
 
-    private static final Logger logger = LogManager.getLogger(ImageController.class);
+    private static final Logger logger = LoggerFactory.getLogger(ImageController.class);
 
     @RequestMapping(value = "/getAll", method = RequestMethod.GET)
     @Transactional
